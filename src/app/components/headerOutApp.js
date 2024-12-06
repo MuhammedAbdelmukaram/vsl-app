001db48d150d729e5a322bfafd440912cd901206
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-12-06 12:39:12 +0100

    first

--- src/app/components/headerOutApp.js
+++ src/app/components/headerOutApp.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import styles from "./HeaderOutApp.module.css";
+import styles from "./headerOutApp.module.css";
 
 const HeaderOutApp = () => {
     const [menuOpen, setMenuOpen] = useState(false);
