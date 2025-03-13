faccde890acdd15c550339525218de640660419c
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2025-03-13 21:08:28 +0100

    Im tired

--- src/app/guide/page.js
+++ src/app/guide/page.js
@@ -3,7 +3,7 @@
 import React, {useState, useEffect} from "react";
 import Select from "react-select";
 import styles from "./Guide.module.css";
-import {selectStyles} from "@/app/components/SelectStyles2";
+import {selectStyles} from "@/app/components/selectStyles2";
 import Image from "next/image";
 import HeaderOutApp from "@/app/components/headerOutApp";
 
